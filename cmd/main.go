60955f94682dba466564a8bf247352cfe4a94d27
Irvig Krehan <irvig.krehan@stone.com.br>
2023-10-19 19:04:48 -0300

    feat: create entrypoint

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"genoise/pkg/noise"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) < 4 {
+		log.Fatal("Missing Arguments")
+	}
+
+	widthInput, heightInput, fileName := os.Args[1], os.Args[2], os.Args[3]
+
+	width, err := strconv.Atoi(widthInput)
+	if err != nil {
+		log.Fatalf("Value is not an integer: %v", widthInput)
+	}
+
+	height, err := strconv.Atoi(heightInput)
+	if err != nil {
+		log.Fatalf("Value is not an integer: %v", heightInput)
+	}
+
+	noise := noise.New(noise.Discrete)
+	err = noise.GenerateImage(fileName, width, height)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}

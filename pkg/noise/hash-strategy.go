0b1b14276f5a5b11a4fffb015692828976196af2
Irvig Krehan <irvig.krehan@stone.com.br>
2023-10-20 17:04:02 -0300

    feat: return only positive values

--- pkg/noise/hash-strategy.go
+++ pkg/noise/hash-strategy.go
@@ -8,6 +8,6 @@ func hashStrategy(x, y int) (float64, error) {
 	xmod, ymod, mod := 0.129898, 0.78233, 43758.5453
 	dot := math.Sin(float64(x)*xmod+float64(y)*ymod) * mod
 
-	_, res := math.Modf(dot)
+	_, res := math.Modf(math.Abs(dot))
 	return res, nil
 }
